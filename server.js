74975f16a5f0f497f9f344dffdec9437fbdc3d22
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2021-01-14 16:50:58 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
+/*
+-------------------------------------------------------------------------------
+A main app server javascript for the burger app
+-------------------------------------------------------------------------------
+*/
+'use strict';
 
-// Node Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
-// Open Server
-var PORT = process.env.PORT || 3000;
+// Load express
+const express = require('express');
+const exphbs = require('express-handlebars');
 
-var app = express();
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
-// app.use(express.static('public'));
+const app = express();
 
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// Load router module(s) and initialize
+const Router = require('./controllers/burgers_controller');
+const router = new Router(app);
 
-// Handlebars
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// Make use of the body-parsers
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set static directory reference path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Start routers
+router.start();
 
-var router = require('./controllers/burgers_controller.js');
-app.use('/', router);
-
-
-
-app.listen(PORT, function() 
-{
-  console.log("App listening on PORT " + PORT);
+// Start the server to listen to the port
+app.listen(PORT, () => {
+  console.log('Server started listening on port ' + PORT);
 });
-© 2021 GitHub, Inc.
