803673278f98ea79633838eb63dd1ed618fdb6f2
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2021-01-14 21:13:25 -0500

    updated Heroku Database

--- config/connection.js
+++ config/connection.js
@@ -21,8 +21,8 @@ const local = {
 // const cleardb = process.env.CLEARDB_DATABASE_URL;
 // const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 
-const jawsdb = process.env.DATABASE_URL;
-const connParams = (process.env.DATABASE_URL) ? jawsdb : local;
+const jawsdb = process.env.JAWSDB_URL;
+const connParams = (process.env.JAWSDB_URL) ? jawsdb : local;
 const connection = mysql.createConnection(connParams);
 
 // Attempt to connecto to the database
